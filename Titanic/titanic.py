016b82ff1d4a935b10c214d6f85787899b3e0db8
hedejing <hedejin95@gmail.com>
2016-03-26 14:15:59 +0800

    add Titanic

---
+++ Titanic/titanic.py
@@ -0,0 +1,127 @@
+import pandas
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+
+titanic = pandas.read_csv('train.csv')
+titanic_test = pandas.read_csv('test.csv')
+
+import re
+
+# A function to get the title from a name.
+def get_title(name):
+    # Use a regular expression to search for a title.  Titles always consist of capital and lowercase letters, and end with a period.
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+    
+# Get all the titles and print how often each one occurs.
+titles = titanic["Name"].apply(get_title)
+print(pandas.value_counts(titles))
+
+# Map each title to an integer.  Some titles are very rare, and are compressed into the same codes as other titles.
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+for k,v in title_mapping.items():
+    titles[titles == k] = v
+
+# Verify that we converted everything.
+print(pandas.value_counts(titles))
+
+# Add in the title column.
+titanic["Title"] = titles
+
+# Generating a familysize column
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+# The .apply method generates a new series
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+
+import operator
+
+# A dictionary mapping family name to id
+family_id_mapping = {}
+
+# A function to get the id given a row
+def get_family_id(row):
+    # Find the last name by splitting on a comma
+    last_name = row["Name"].split(",")[0]
+    # Create the family id
+    family_id = "{0}{1}".format(last_name, row["FamilySize"])
+    # Look up the id in the mapping
+    if family_id not in family_id_mapping:
+        if len(family_id_mapping) == 0:
+            current_id = 1
+        else:
+            # Get the maximum id from the mapping and add one to it if we don't have an id
+            current_id = (max(family_id_mapping.items(), key=operator.itemgetter(1))[1] + 1)
+        family_id_mapping[family_id] = current_id
+    return family_id_mapping[family_id]
+
+# Get the family ids with the apply method
+family_ids = titanic.apply(get_family_id, axis=1)
+
+# There are a lot of family ids, so we'll compress all of the families under 3 members into one code.
+family_ids[titanic["FamilySize"] < 3] = -1
+
+# Print the count of each unique id.
+print(pandas.value_counts(family_ids))
+
+titanic["FamilyId"] = family_ids
+
+
+# First, we'll add titles to the test set.
+titles = titanic_test["Name"].apply(get_title)
+# We're adding the Dona title to the mapping, because it's in the test set, but not the training set
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2, "Dona": 10}
+for k,v in title_mapping.items():
+    titles[titles == k] = v
+titanic_test["Title"] = titles
+# Check the counts of each unique title.
+print(pandas.value_counts(titanic_test["Title"]))
+
+# Now, we add the family size column.
+titanic_test["FamilySize"] = titanic_test["SibSp"] + titanic_test["Parch"]
+
+# Now we can add family ids.
+# We'll use the same ids that we did earlier.
+print(family_id_mapping)
+
+family_ids = titanic_test.apply(get_family_id, axis=1)
+family_ids[titanic_test["FamilySize"] < 3] = -1
+titanic_test["FamilyId"] = family_ids
+#add the NameLength column to titanic_test
+titanic_test["NameLength"] = titanic_test["Name"].apply(len)
+
+
+
+predictors = ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+
+algorithms = [
+    [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), predictors],
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+]
+
+full_predictions = []
+for alg, predictors in algorithms:
+    # Fit the algorithm using the full training data.
+    alg.fit(titanic[predictors], titanic["Survived"])
+    # Predict using the test dataset.  We have to convert all the columns to floats to avoid an error.
+    predictions = alg.predict_proba(titanic_test[predictors].astype(float))[:,1]
+    full_predictions.append(predictions)
+
+# The gradient boosting classifier generates better predictions, so we weight it higher.
+predictions = (full_predictions[0] * 3 + full_predictions[1]) / 4
+
+# Turn the predictions into either 0 or 1
+predictions[predictions > .5] = 1
+predictions[predictions <= .5] = 0
+# convert the predictions to integers
+predictions = predictions.astype(int)
+# create a submission dataframe
+submission = pandas.DataFrame({
+        "PassengerId" : titanic_test["PassengerId"],
+        "Survived" : predictions
+    })
+    
+submission.to_csv("kaggle.csv", index=False)
